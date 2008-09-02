90af6ff2dfa04b2a574a19df1b78c0a1d8c42005
emax <emax@f708bf84-5827-0410-8242-ae4f0cc3d000>
2008-09-02 18:34:42 +0000

    changed wording

--- jv3/study/emails.py
+++ jv3/study/emails.py
@@ -7,21 +7,24 @@ First, THANK YOU for helping us test and find bugs in list.it in its early phase
 
 With list.it finally in good shape, we are launching our note-taking study this Wednesday.
 
-Since we've revised the study considerably signed up for previously, we need you to tell us if you would still like to participate.  If you don't respond, we will assume that you no longer wish to participate in the study and won't pester you about it any more.
+Since we've revised the study considerably signed up for previously, we need you to tell us if you would still like to participate.  If you don't respond, we will assume that you no longer wish to participate.
 
 Here is a summary of the changes:
 
-1. Notetaking exercises:  We're going to send you an e-mail with a link containing some information we want you to capture into a note in list.it twice a day for ten days (10 days), starting Wednesday. For example, we might ask you to pretend that you need to remind yourself to pick up groceries for dinner on the way home -- and to write it down in list.it. Each exercise should only take a minute or two at most to complete.
+1. Notetaking exercises:   We're going to send you an e-mail with a link containing some information we want you to capture into a note in list.it, twice a day for ten days starting Wednesday. For example, we might ask you to pretend that you need to remind yourself to pick up groceries for dinner on the way home -- and to write down the thought in list.it. Each exercise should only take a minute or two at most to complete.
 
-2. Monetary rewards: For each exercise you complete, you get $1.  With twenty exercises delivered over ten days, you can earn $20.  Plus, you could win additional big prizes (which will be posted on our web site shortly) if you are one of the top three list.it note-keepers (for your own notes) during the duration of the study (10 days).
+2. Monetary rewards:  For each exercise you complete, you get $1.  With twenty exercises delivered over ten days, you can earn $20.  Plus, you could win an additional $50 (1st place) or $25 (two 2nd place prizes) if you use list.it for your own notes, and turn out to be one of the top three note-takers in the study.
 
-If you want to participate in this new study, please click here:  %(server_url)s/jv3/reconsent?email=%(email)s
+If you want to participate in this new study, please click here: %(server_url)s/jv3/reconsent?email=%(email)s
+
 If you wish to withdraw from the study at any time, you can write us at listit@csail.mit.edu. 
 
 And don't forget to please continue using list.it!  List.it is intended for you to capture your thoughts and notes in a convenient way, and at the same time aid PIM research towards helping people build better tools.  Help us help you with your information overload.
 
-Thanks,
+Thank you,
+
 the list.it team
+
 electronic Max, Michael Bernstein, Greg Vargas, Professor David Karger
 Haystack Group, MIT CSAIL
 listit@csail.mit.edu
