f716224ceb2c80236343f973ee3669371fb3221a
emax <emax@f708bf84-5827-0410-8242-ae4f0cc3d000>
2008-09-02 07:04:48 +0000

    changed wording

--- jv3/study/emails.py
+++ jv3/study/emails.py
@@ -13,7 +13,7 @@ Here is a summary of the changes:
 
 1. Notetaking exercises:  We're going to send you an e-mail with a link containing some information we want you to capture into a note in list.it twice a day for ten days (10 days), starting Wednesday. For example, we might ask you to pretend that you need to remind yourself to pick up groceries for dinner on the way home -- and to write it down in list.it. Each exercise should only take a minute or two at most to complete.
 
-2. Monetary rewards: For each exercise you complete, you get $1.  With twenty exercises delivered over ten days, you can earn $20.  Plus, you could win an additional $50 (1st place) or $25 (2nd, 3rd places) if you use list.it for your own notes, and turn out to be one of the top three note-takers during the duration of the study (10 days).
+2. Monetary rewards: For each exercise you complete, you get $1.  With twenty exercises delivered over ten days, you can earn $20.  Plus, you could win additional BIG prizes (which will be posted on our web site shortly) if you are one of the top three list.it note-keepers (for your own notes) during the duration of the study (10 days).
 
 If you want to participate in this new study, please click here:  %(server_url)s/jv3/reconsent?email=%(email)s
 If you wish to withdraw from the study at any time, you can write us at listit@csail.mit.edu. 
