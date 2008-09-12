caa07b61dbf35b0ed39ccb417d4dda316d9f50bf
emax <emax@f708bf84-5827-0410-8242-ae4f0cc3d000>
2008-09-12 23:10:43 +0000

    survey logic sort of working!

--- jv3/study/exporter.py
+++ jv3/study/exporter.py
@@ -1,7 +1,7 @@
 
 import django.contrib.auth.models as authmodels
 import jv3.utils
-import time
+import time,datetime
 import jv3.models
 import re
 
@@ -24,27 +24,30 @@ i can't afford a carriage."""
 ## print "number of lines %d " % nlp_count_lines(TEST_STRING)
 ## print "avg word length %g " % nlp_average_word_length(TEST_STRING)
 
-activity_logs_for_note = lambda note: jv3.models.ActivityLog(action="note-edit",owner=n.owner,noteid=n.jid)
+activity_logs_for_note = lambda n,action="note-edit": jv3.models.ActivityLog.objects.filter(action=action,owner=n.owner,noteid=n.jid)
+delete_log_for_note = lambda n: jv3.models.ActivityLog.objects.filter(action="note-delete",owner=n.owner,noteid=n.jid)
 note_n_edits = lambda note:len( activity_logs_for_note(note) )
+note_n_saves = lambda note:len( activity_logs_for_note(note,action="note-save") )
 
 def note_total_edit_time(note):
     nlogs = activity_logs_for_note(note)
     nlogs.sort(key=lambda log : log['id'])
     ## determine consecutiveness
-    
     pass
+
+def note_deleted_time(note):
+    d = delete_log_for_note(note)
+    if len(d) > 0: return time.ctime(long(d[0].when)/1000)
+    return None
+
 def note_avg_edit_time(note):
     pass
-
-def note_lifetime(note):
-    pass
-
 
 ## string statistics
 str_n_words = lambda s: len( [l for l in s.split() if len(l.strip()) > 0] )
 str_n_lines = lambda s: len([l for l in s.split('\n') if len(l.strip()) > 0])
-
 str_n_urls = lambda s: len([ x.start for x in re.finditer('http:[\S]+',s)])
+
 def str_is_probe_response(s):
     hits = [ e for e in re.finditer('note (\d+)', s, re.IGNORECASE) ]
     if len(hits) > 0: return hits[0].groups()[0]
@@ -60,9 +63,10 @@ def str_average_word_length(s):
 note_guid = lambda note: note.id
 note_jid = lambda note: note.jid
 note_owner_email = lambda note: note.owner.email
+note_owner_id = lambda note: note.owner.id
 note_deleted = lambda note: repr(bool(note.deleted))
-note_created = lambda note: time.ctime(int(note.created)/1000)
-note_modified = lambda note: time.ctime(int(note.edited)/1000)
+note_created = lambda note: time.ctime(long(note.created)/1000)
+note_modified = lambda note: time.ctime(long(note.edited)/1000)
 note_version = lambda note: repr(int(note.version))
 note_contents = lambda note: defang(note.contents)
 note_contents_length = lambda note: len(note.contents)
@@ -73,25 +77,40 @@ note_n_urls = lambda note: str_n_urls(note.contents)
 note_is_probe_response = lambda note:str_is_probe_response(note.contents)
 note_avg_word_length = lambda note:str_average_word_length(note.contents)
 
+def note_lifetime(n):
+    endtime = time.time()
+    if n.deleted:
+        d = delete_log_for_note(n)
+        if len(d) > 0:
+            endtime = long(d[0].when)/1000
+    return endtime - long(n.created)/1000
+
 def makestr(v):
     if isinstance(v,str):
         return v
     return repr(v)
 
-note_statistic_fns = [  ('guid',note_guid),
-                     ('jid',note_jid),
-                     ('user',note_owner_email),
-                     ('deleted',note_deleted),
-                     ('created_time',note_created),
-                     ('modified_time',note_modified),
-                     ('version',note_version),
-                     ('contents',note_contents),
-                     ('length',note_contents_length),
-                     ('n_words',note_n_words),
-                     ('n_lines',note_n_lines),
-                     ('n_urls',note_n_urls),
-                     ('is_probe_response',note_is_probe_response),
-                     ('avg_word_length',note_avg_word_length) ]    
+note_statistic_fns = [
+    ('guid',note_guid),
+    ('jid',note_jid),
+    ('user',note_owner_email),
+    ('user_id',note_owner_id),
+    ('deleted',note_deleted),
+    ('contents',note_contents),
+    ('created_time',note_created),
+    ('modified_time',note_modified),
+    ('version',note_version),
+    ('is_probe_response',note_is_probe_response),
+    ('length',note_contents_length),
+    ('n_words',note_n_words),
+    ('avg_word_length',note_avg_word_length),
+    ('n_lines',note_n_lines),
+    ('n_urls',note_n_urls),
+    ('n_edit_select',note_n_edits),
+    ('n_edit_save',note_n_saves),
+    ('delete_time',note_deleted_time),
+    ('lifetime',note_lifetime),
+]
 
 def make_spreadsheet(stat_rows, field_delim="\t", row_delim="\n", col_headers=None):
     ## col headers
@@ -113,7 +132,7 @@ def notes_statistics(users=None, cols=None, stat_fns=note_statistic_fns):
 
        
 #print notes(authmodels.User.objects.filter(email="emax@csail.mit.edu"))
-print make_spreadsheet(notes_statistics(),col_headers=[x[0] for x in note_statistic_fns])
+#print make_spreadsheet(notes_statistics(),col_headers=[x[0] for x in note_statistic_fns])
    
 
 
--- jv3/study/survey.py
+++ jv3/study/survey.py
@@ -2,8 +2,11 @@
 import jv3.utils
 import jv3.study.exporter
 
-def make_header(s):
-    return { "text" : "<h3>%s</h3>" % s }
+TXT = "textfield"
+FR = "textarea"
+MC = "radio"
+MS = "checkbox"
+
 
 def show_probe_note(email,jid):
     n = jv3.utils.get_note_by_email_and_jid(email,jid)
@@ -14,30 +17,33 @@ On note %d, you took down the following information:
 <div class=\"note\">%s</div>
 """ % (responseid, n.contents) }
 
-common_questions = {
-    "gen": make_header("General"),
-    "per": make_header("Personal"),
-}
-questions_by_user = {
-    "emax@csail.mit.edu": [  common_questions["gen"],
-                               show_probe_note('emax@csail.mit.edu', '924659'),
-                               common_questions["per"],                               
-                               { "qid" : "q1",
-                                 "type" : "freeresponse",
-                                 "text" : "How did you like list.it" },
-                               { "qid" : "q2",
-                                 "type" : "freeresponse",
-                                 "text" : "What did you do today?" },
-                               { "qid" : "q3",
-                                 "type" : "multiplechoice",
-                                 "text" : "What is your favorite variety of coffee",
-                                 "values" : ["drip", "espresso", "turkish/greek", "dry grounds"] },
-                               { "qid" : "q4",
-                                 "type" : "multipleselect",
-                                 "text" : "What do you do every day",
-                                 "values" : ["Sleep", "eat", "attend seminars", "write chi papers"] },
-                               ],
-}
-    
-      
-      
+def make_header(s):
+    return { "text" : "<h3>%s</h3>" % s }
+
+def make_question(id,question,type,choices=None,default_val="",choice_other=False):
+    templ = {"qid" : id,
+             "type": type,
+             "text": question}
+    if (choices) : templ["values"] = choices;
+    if (default_val) : templ["response"] = default_val
+    if (choice_other) : templ["other"] = True
+    return templ
+
+common_questions = [
+    make_header("Background"),
+    make_question("b0", "Your gender", MC, ["Male","Female","N/A"]),
+    make_question("b1", "Your age", MC, ["<18","18-25","25-30", "30-35", "35-40", "40-50", "50-65", "65+"]),
+    make_question("b2", "Your occuptation/role", MC,
+                 ["Student","Engineer","Manager (Product/Sales/Services)", "Advertising", "Artist/Writer", "Journalist", "Musician"], choice_other=True),
+    make_question("b3", "Your computers", MS,["Home PC","Work PC", "Laptop", "Cell"], choice_other=True),
+    make_question("b4", "Your favorite color", FR),    
+    make_header("Personal"),
+]
+
+questions_by_user = {}
+
+for u in ['emax@csail.mit.edu']:
+    qs = [];
+    for q in common_questions: qs.append(q);
+    questions_by_user[u] = qs;
+                               
