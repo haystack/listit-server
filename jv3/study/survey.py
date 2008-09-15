869acf640910b955ee4e39a8d477ba81a8571f11
emax <emax@f708bf84-5827-0410-8242-ae4f0cc3d000>
2008-09-15 04:57:04 +0000

    patch

--- jv3/study/survey.py
+++ jv3/study/survey.py
@@ -291,10 +291,11 @@ def get_survey_for_user(u,limit=settings.SURVEY_MAX_NOTES_PER_QUESTION):
 def get_survey_takers():
     whitelist = load_whitelist_csv(settings.SURVEY_FR_NOTELIST)
     incomplete_users = []
+    consenting = get_consenting_users()
     for e in whitelist.iterkeys():
         u = authmodels.User.objects.filter(email=e)
         if not u: continue
-        if len(jv3.models.SurveyDoneDeclaration.objects.filter(user=u[0])) == 0:
+        if u in consenting and len(jv3.models.SurveyDoneDeclaration.objects.filter(user=u[0])) == 0:
             incomplete_users.append(u)
     return incomplete_users
             
