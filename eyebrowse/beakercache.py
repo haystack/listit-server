973820a83ba1ba065c649e401f75fd3f34b19534
emax <emax@f708bf84-5827-0410-8242-ae4f0cc3d000>
2009-09-08 21:20:18 +0000

    re-adding

---
+++ eyebrowse/beakercache.py
@@ -0,0 +1,24 @@
+from beaker.cache import CacheManager
+from beaker.util import parse_cache_config_options
+
+CACHE_EXPIRY = 60 ## for debug 
+
+cache_opts = {
+    'cache.type': 'file',
+    'cache.data_dir': '/var/eyebrowse/workspace/trunk/server/eyebrowse/cache/data',
+    'cache.lock_dir': '/var/eyebrowse/workspace/trunk/server/eyebrowse/cache/lock',
+    'cache.regions': 'short_term, long_term, to_from_url, top_users_long_term',
+    'cache.short_term.type': 'file',
+    # comment above line and uncomment 2 below lines to use memcached
+    #'cache.short_term.type': 'ext:memcached', 
+    #'cache.short_term.url': '127.0.0.1.1121', 
+    'cache.short_term.expire': CACHE_EXPIRY, #'7200', # 6 hours
+    'cache.long_term.type': 'file',
+    'cache.long_term.expire': CACHE_EXPIRY, #'43200', # 12 hours '86400', 1 day
+    'cache.to_from_url.type': 'file',
+    'cache.to_from_url.expire': CACHE_EXPIRY, #'43299', # 12 hours '86400', 1 day
+    'cache.top_users_long_term.type': 'file',
+    'cache.top_users_long_term.expire': CACHE_EXPIRY, #'43200', # 12 hours '86400', 1 day
+    }
+
+cache = CacheManager(**parse_cache_config_options(cache_opts))
