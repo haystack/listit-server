92d7e9667e81c0932a02cb14dd3f966dc491c4d6
brennanmoore <brennanmoore@f708bf84-5827-0410-8242-ae4f0cc3d000>
2009-09-09 16:58:14 +0000

    beachercache now looks a bit nicer, easier to change things

--- eyebrowse/beakercache.py
+++ eyebrowse/beakercache.py
@@ -1,7 +1,10 @@
 from beaker.cache import CacheManager
 from beaker.util import parse_cache_config_options
 
-CACHE_EXPIRY = 43200 ## for debug 
+CACHE_EXPIRY = '60' ## for debug 
+CACHE_EXPIRY_ST = '3600' ## 3 hours
+CACHE_EXPIRY_LT = '43200' ## 12 hours
+
 
 cache_opts = {
     'cache.type': 'file',
@@ -12,13 +15,13 @@ cache_opts = {
     # comment above line and uncomment 2 below lines to use memcached
     #'cache.short_term.type': 'ext:memcached', 
     #'cache.short_term.url': '127.0.0.1.1121', 
-    'cache.short_term.expire': CACHE_EXPIRY, #'7200', # 6 hours
+    'cache.short_term.expire': CACHE_EXPIRY_ST,
     'cache.long_term.type': 'file',
-    'cache.long_term.expire': CACHE_EXPIRY, #'43200', # 12 hours '86400', 1 day
+    'cache.long_term.expire': CACHE_EXPIRY_LT,
     'cache.to_from_url.type': 'file',
-    'cache.to_from_url.expire': CACHE_EXPIRY, #'43299', # 12 hours '86400', 1 day
+    'cache.to_from_url.expire': CACHE_EXPIRY_LT,
     'cache.top_users_long_term.type': 'file',
-    'cache.top_users_long_term.expire': CACHE_EXPIRY, #'43200', # 12 hours '86400', 1 day
+    'cache.top_users_long_term.expire': CACHE_EXPIRY_LT,
     }
 
 cache = CacheManager(**parse_cache_config_options(cache_opts))
