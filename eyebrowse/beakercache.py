fb6df60d47ab3c08735b381c79351c8e367571cc
brennanmoore <brennanmoore@f708bf84-5827-0410-8242-ae4f0cc3d000>
2009-09-10 06:59:20 +0000

    making sure beacher changes are checked in

--- eyebrowse/beakercache.py
+++ eyebrowse/beakercache.py
@@ -1,9 +1,11 @@
 from beaker.cache import CacheManager
 from beaker.util import parse_cache_config_options
 
-CACHE_EXPIRY = '60' ## for debug 
-CACHE_EXPIRY_ST = '3600' ## 3 hours
-CACHE_EXPIRY_LT = '43200' ## 12 hours
+CACHE_EXPIRY = '3600' ## for debug
+CACHE_EXPIRY_ST = '3600' ## for debug
+CACHE_EXPIRY_LT = '3600' ## for debug 
+#CACHE_EXPIRY_ST = '3600' ## 3 hours
+#CACHE_EXPIRY_LT = '43200' ## 12 hours
 
 
 cache_opts = {
@@ -11,16 +13,16 @@ cache_opts = {
     'cache.data_dir': '/var/eyebrowse/workspace/trunk/server/eyebrowse/cache/data',
     'cache.lock_dir': '/var/eyebrowse/workspace/trunk/server/eyebrowse/cache/lock',
     'cache.regions': 'short_term, long_term, to_from_url, top_users_long_term',
-    'cache.short_term.type': 'file',
+    'cache.short_term.type': 'memory',
     # comment above line and uncomment 2 below lines to use memcached
     #'cache.short_term.type': 'ext:memcached', 
     #'cache.short_term.url': '127.0.0.1.1121', 
     'cache.short_term.expire': CACHE_EXPIRY_ST,
-    'cache.long_term.type': 'file',
+    'cache.long_term.type': 'memory',
     'cache.long_term.expire': CACHE_EXPIRY_LT,
-    'cache.to_from_url.type': 'file',
+    'cache.to_from_url.type': 'memory',
     'cache.to_from_url.expire': CACHE_EXPIRY_LT,
-    'cache.top_users_long_term.type': 'file',
+    'cache.top_users_long_term.type': 'memory',
     'cache.top_users_long_term.expire': CACHE_EXPIRY_LT,
     }
 
