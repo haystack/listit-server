8bdc9efdec30f661f08545158241f54cca21d58a
emax <emax@f708bf84-5827-0410-8242-ae4f0cc3d000>
2009-09-11 01:09:14 +0000

    added code for sending survey

--- jv3/study/study2_survey.py
+++ jv3/study/study2_survey.py
@@ -7,6 +7,57 @@ em = User.objects.filter(email="emax@csail.mit.edu")[0]
 
 INTERVIEWEES = ['emmanuel.ruellan@laposte.net', 'kubonagarei@gmail.com', 'LDD@LDowning.com', 'molly.haber@gmail.com', 'camera1776@yahoo.com', 'amykolo@brandeis.edu', 'jameschu7@gmail.com', 'manishgarg21@gmail.com', 'jamey.hicks@nokia.com', 'tonysr@gmail.com', 'listit@myhisham.otherinbox.com', 'tedbent@earthlink.net', 'adam.simantel@gmail.com', 'rgdarch@aol.com', 'gdtschuller@gmail.com', 'jleecasler@gmail.com', 'kennyosh@gmail.com', 'freemanj@columbus.rr.com', 'justacopy@gmail.com', 'jpjporras0@gmail.com', 'pascal.lemerrer@wanadoo.fr', 'leifer@fas.harvard.edu', 'jillar@mit.edu', 'henry.chang@dmjmhn.aecom.com', 'underthearrow@gmail.com', 'deepak@somatv.com', 'med@ipcbilling.com', 'mcgaheec@swbell.net', 'jjordan8@mac.com', 'cartoonita@yahoo.fr']
 
+INTERVIEWEES = [l.strip().lower() for l in INTERVIEWEES]
+
+PILOTS = ['ray@cornbill.com',
+          'emax@csail.mit.edu',
+          'bsfishdm@hotmail.com',
+          'kirk.etheridge@gmail.com',
+          'kaelia@gmail.com',
+          'markito3@gmail.com',
+          'm.liu.jin@gmail.com',
+          'k.blattmann@gmail.com',
+          'electronic@gmail.com',
+          'ch.herrera@gmail.com',
+          'jbarilla@cs.princeton.edu',
+          'JoshuaEstrada@gmail.com',
+          'msbernst@mit.edu',
+          'anhxakeng@gmail.com',
+          '303frankel@gmail.com',
+          'vanderhide@aol.com',
+          'albina.shatzman@gmail.com',
+          'sjpschmid@gmail.com',
+          'leaffan@gmx.net',
+          'jmr@computing.com',
+          'rosen.ed@gmail.com',
+          'glittle@gmail.com',
+          'cgarcia@acm.org',
+          'amirkovich@mindspring.com',
+          'glenda@herzog-cpa.com',
+          'betsylindley@yahoo.com',
+          'michaelburt@spamcop.net',
+          'dmdadnwn@gmail.com',
+          'lonewol@yahoo.com',
+          'volavar@gmail.com',
+          'sharudinj@gmail.com',
+          'bomfog@gmail.com',
+          'racvac@gmail.com',
+          'plhodson@gmail.com',
+          'pico@gmx.us',
+          'e.saunier@reseau2000.net',
+          'still23@op.pl',
+          'tim@cygnus.co.nz',
+          'jlove0817@yahoo.com',
+          'jasonm@csail.mit.edu',
+          'caldwell.gary@gmail.com',
+          'thibaud.antignac@gmail.com',
+          'katfisch@mit.edu',
+          'quadrafonix@yahoo.com',
+          'junees@web.de',
+          'idro@live.com',
+          'laurentbertrand@hotmail.com',
+          'buzlinky@gmail.com']
+
 EMAIL_SUBJECT = "Complete a short survey for List-It, chance to win $20!"
 
 
@@ -36,9 +87,11 @@ Prof. David Karger and the List-It Team
 
 TO_SEND = None
 
-def _compute_send :
+def _compute_send() :
     global TO_SEND
-    TO_SEND = set( [ u for u in User.objects.all() if is_consenting_study2(u) and (u.note_owner.all().count() > 10) and not (u.email in INTERVIEWEES) ] )
+    TO_SEND = set( [ u for u in User.objects.all() if is_consenting_study2(u) and (u.note_owner.all().count() > 10)
+                     and not (u.email.strip().lower() in INTERVIEWEES) ] )
+    return TO_SEND
 
 def test_send_plea():
     email_users([em],EMAIL_SUBJECT,EMAIL)
@@ -47,11 +100,20 @@ def send_pilots():
     global TO_SEND
     if TO_SEND is None:
         _compute_send()
-    pilots = TO_SEND[0:30]
+    pilots = list(TO_SEND)[0:48]
+    #print pilots
     email_users(pilots,EMAIL_SUBJECT,EMAIL)
 
 
-    
+def send_everyone():
+    global TO_SEND
+    global PILOTS
+    if TO_SEND is None:
+        _compute_send()
+    everyone = [ u for u in TO_SEND if not u.email.strip().lower() in PILOTS ]
+    email_users(pilots,EMAIL_SUBJECT,EMAIL)
+
+
 
 
 
