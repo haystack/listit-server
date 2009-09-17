48e2e27e00da9d6ce5d3f9f1e7650a490be0c8d4
emax <emax@f708bf84-5827-0410-8242-ae4f0cc3d000>
2009-09-17 07:30:46 +0000

    lots of notes and better search stuff

--- jv3/study/ca_search.py
+++ jv3/study/ca_search.py
@@ -4,6 +4,8 @@ from django.utils.simplejson import JSONDecoder
 
 search_cache = {}
 search_query_cache = {}
+all_searches = []
+all_queries = []
 
 def user_search(user,days_ago=None):
     from jv3.study.content_analysis import activity_logs_for_user
@@ -23,36 +25,49 @@ def user_search(user,days_ago=None):
                 continue
             if type(query) == dict:
                 if 'search' in query:
-                    queries.append(query["search"])
-                    searches.append(al)
+                    # no empty searches pls
+                    if len(query["search"].strip()) > 0:
+                        queries.append(query["search"])
+                        searches.append(al)
         elif al["action"] == 'clear-search':
             searches.append(al)
 
     search_cache[user.id] = searches
     search_query_cache[user.id] = queries
 
-def get_searches(notes):
+
+def get_searches(users):
     global search_cache
     global search_query_cache
+    global all_searches
+    global all_queries
 
-    for note in notes:
-        if note["owner"].id not in search_cache:
-            user_search(note["owner"])
+    for user in users:
+        user_search(user)
 
-    all_queries = []
-    for user in search_query_cache:
+    for user in search_cache:
+        for search in search_cache[user]:
+            all_searches.append(search)
         for query in search_query_cache[user]:
             all_queries.append(query)
 
-    return all_queries
+def words_per_search():
+    global all_queries
 
-"""
-    user_query_count = {}
+    wps = [len(q.split()) for q in all_queries]
+
+    return wps
+
+def times_repeated():
+    global search_query_cache
+
+    tr = {}
     for user in search_query_cache:
-        user_query_count[user] = {}
+        tr[user] = {}
         for query in search_query_cache[user]:
-            if query in all_queries:
-                all_queries[query] += 1
+            if query not in tr[user]:
+                tr[user][query] = 1
             else:
-                all_queries
-"""
+                tr[user][query] += 1
+
+    return tr
