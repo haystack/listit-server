482813159981c21670d2835da3bb3a8e8010903e
brennanmoore <brennanmoore@f708bf84-5827-0410-8242-ae4f0cc3d000>
2009-09-26 09:43:05 +0000

    updating view.py to link pageview and event

--- eyebrowse/views.py
+++ eyebrowse/views.py
@@ -11,3 +11,16 @@ from django.template import RequestContext
 from django.db.models import Q
 from eyebrowse.forms import *
 from eyebrowse.models import *
+
+## hook for creating relevant Page objects when new jv3.Event objects get created                                     
+## by the listit server (which answers calls from listit)                                                             
+def create_www_pageviews_for_each_event(sender, created=None, instance=None, **kwargs):
+    # print "post-save event for sender %s : %s " % (repr(sender),repr(instance.entityid)) ## debug!!                 
+    if (created and instance is not None):
+        if instance.entitytype == "schemas.Webpage" and instance.entityid is not None:
+            ## print "post-save url: %s " % instance.entityid ## debug!!                                              
+            pageview = PageView.from_Event(instance)
+            # print "Saving %s " % repr(pageview)                                                                     
+            pageview.save()
+
+post_save.connect(create_www_pageviews_for_each_event, sender=Event)
