405d4881528080f0c0547df46e647970b960b32b
brennanmoore <brennanmoore@f708bf84-5827-0410-8242-ae4f0cc3d000>
2009-09-26 07:40:19 +0000

    some more refactoring, lots less code and fewer ajax calls, may be slower for a bit not sure

--- eyebrowse/json/urls.py
+++ eyebrowse/json/urls.py
@@ -11,10 +11,9 @@ urlpatterns = patterns('',
                        (r'^get_views_user/(\w+)/$', get_views_user_json),
                        (r'^get_most_shared_hosts/(\d+)/$', get_most_shared_hosts),
                        (r'^get_recent_web_page_views_user/(\w+)/(\d+)/$', get_recent_web_page_view_user),
-                       (r'^get_top_users_for_url/(\d+)/$', get_top_users_for_url),
-                       (r'^get_to_from_url/(\d+)/$', get_to_from_url_plugin), #for old plugin
-                       (r'^get_hourly_daily_top_urls_user/(\w+)/(\d+)/$', get_hourly_daily_top_urls_user),
                        (r'^get_most_recent_urls/(\d+)/$', get_most_recent_urls),
+                       (r'^get_top_users_for_url/(\d+)/$', get_top_users_for_url),
+                       (r'^get_hourly_daily_top_urls_user/(\w+)/(\d+)/$', get_hourly_daily_top_urls_user),
                        (r'^get_following_views/(\w+)/$', get_following_views),
                        (r'^get_top_hosts_comparison_friends/(\w+)/(\d+)/$', get_top_hosts_comparison_friends),
 
@@ -24,7 +23,9 @@ urlpatterns = patterns('',
                        (r'^get_ticker$', get_ticker),                      
                        (r'^get_users_page$', get_users_page),                      
                        (r'^get_profile$', get_profile),                                            
-                       (r'^get_pagestats$', get_pagestats),                                            
+                       (r'^get_pagestats$', get_pagestats),                       
+
+                       (r'^get_to_from_url/(\d+)/$', get_to_from_url_plugin), #for old plugin                     
 )
 
     
