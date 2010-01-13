b2dd506632a7b8804b2f0727098abf3f3e03f80e
emax <emax@f708bf84-5827-0410-8242-ae4f0cc3d000>
2010-01-13 21:00:40 +0000

    ca_user

--- jv3/study/ca_user.py
+++ jv3/study/ca_user.py
@@ -12,7 +12,7 @@ import rpy2
 import rpy2.robjects as ro
 from jv3.study.study import *
 
-class StudyUser:    
+class PerUser:    
     def __init__(self,user):
         self.u = user
         self._compute_duration()
@@ -26,14 +26,14 @@ class StudyUser:
     def consenting(self):
         return is_consenting_study2(self.u)
     
-    def get_duration(self):
+    def duration(self):
         return (self.start,self.end)
 
     def days_of_use(self):
-        (start,end) = self.get_duration()
+        (start,end) = self.duration()
         return (end-start)/(1.0*24*3600*1000)
 
-    def get_actions(self,n=None):
+    def actions(self,n=None):
         if n:
             return ActivityLog.objects.filter(owner=self.u,noteid=n.jid)
         return ActivityLog.objects.filter(owner=self.u)
@@ -41,22 +41,36 @@ class StudyUser:
     def notes(self):
         return Note.objects.filter(owner=self.u).order_by('created')
 
-    def get_created_per_day(self):
-        start,end = self.get_duration()
-        n = self.notes()
+    def deleted_per_day(self):
+        start,end = self.duration()
+        actions = ActivityLog.objects.filter(action="note-delete",owner=self.u)
+        del_per_day = []
+        while start < end:
+            start = start + 24*60*60*1000
+            dayend = start + 24*60*60*1000
+            del_per_day.append(actions.filter(when__gte=start,when__lte=dayend).count())
+            start = dayend
+        return del_per_day
+
+    def created_per_day(self):
+        start,end = self.duration()
+        n = ActivityLog.objects.filter(action="note-add",owner=self.u)        
         notes_per_day = []
         while start < end:
             start = start + 24*60*60*1000
             dayend = start + 24*60*60*1000
-            notes_per_day.append(n.filter(start__gte=start,start__lte=dayend).count())
+            notes_per_day.append(n.filter(when__gte=start,when__lte=dayend).count())
             start = dayend
         return notes_per_day
+    
 
+## static methods
+    
 def users_for_more_than_n_days(min_days,out_of,maxN=None):
     if out_of is None: out_of = [u for u in User.objects.all() if is_consenting_study2(u)]
     results = []
     for x in out_of:
-        su = StudyUser(x)
+        su = PerUser(x)
         if su.days_of_use() >= min_days:
             results.append(su)
         if maxN is not None and len(results) > maxN:
