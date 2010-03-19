40d86f0bdbb036e577791ae57f83634870e7bd8d
zamiang <zamiang@f708bf84-5827-0410-8242-ae4f0cc3d000>
2010-03-19 06:03:00 +0000

    updates to the eyebrowse settings page

--- eyebrowse/lib/js/settings.js
+++ eyebrowse/lib/js/settings.js
@@ -1,375 +1,76 @@
-var pageList = {
-        "news": [
-            "nytimes.com",
-            "abcnews.go.com",
-            "alarabiya.net",
-            "news.aol.com",
-            "hosted.ap.org",
-            "news.bbc.co.uk",
-            "cbc.ca",
-            "cbsnews.com",
-            "csmonitor.com",
-            "cnn.com",
-            "spiegel.de",
-            "ctv.ca/news",
-            "economist.com",
-            "euronews.net",
-            "foxnews.com",
-            "france24.com",
-            "news.google.com",
-            "guardian.co.uk",
-            "iht.com",
-            "mcclatchydc.com",
-            "msnbc.msn.com",
-            "news.com",
-            "npr.org",
-            "oneworld.net",
-            "latimes.com",
-            "pbs.org",
-            "radionetherlands.nl",
-            "reuters.com",
-            "rte.ie",
-            "news.sky.com",
-            "timesonline.co.uk",
-            "topix.com",
-            "un.org",
-            "usatoday.com",
-            "voanews.com",
-            "wsj.com",
-            "washingtonpost.com",
-            "en.wikinews.org",
-            "wnnetwork.com",
-            "news.yahoo.com",
-            "news.google.com",
-            "espn.go.com",
-            "msn.foxsports.com",
-            "huffingtonpost.com",
-            "drudgereport.com",
-            "foxnews.com",
-            "ign.com",
-            "gamefaqs.com",
-            "digg.com",
-            "engadget.com",
-            "techmeme.com",
-            "wired.com",
-            "slashdot.org",
-            "cnet.com",
-            "microsoft.com",
-            "apple.com",
-            "time.com",
-            "buzz.yahoo.com",
-            "reddit.com",
-            "delicious.com",
-            "diigo.com",
-            "newsvine.com",
-            "fark.com",
-            "propeller.com",
-            "mixx.com",
-            "stumbleupon.com" 
-        ],
-        "blogs": [
-            "bloglines.com",
-            "blogger.com",
-            "wordpress.com",
-            "typepad.com",
-            "livejournal.com",
-            "woostercollective.com",
-            "boingboing.net",
-            "technorati.com",
-            "techcrunch.com",
-            "mashable.com",
-            "ffffound.com",
-            "booooooom.com",
-            "butdoesitfloat.com",
-            "gizmodo.com",
-            "googleblog.blogspot.com",
-            "lifehacker.com",
-            "arstechnica.com",
-            "tmz.com",
-            "readwriteweb.com",
-            "gawker.com",
-            "smashingmagazine.com",
-            "sethgodin.typepad.com",
-            "whitehouse.gov/blog",
-            "kotaku.com",
-            "thedailybeast.com",
-            "dailykos.com",
-            "andrewsullivan.theatlantic.com",
-            "consumerist.com",
-            "treehugger.com",
-            "corner.nationalreview.com",
-            "crunchgear.com",
-            "bits.blogs.nytimes.com",
-            "thinkprogress.org",
-            "icanhascheezburger.com",
-            "gmailblog.blogspot.com",
-            "perezhilton.com",
-            "blogs.abcnews.com",
-            "politicalticker.blogs.cnn.com",
-            "autoblog.com",
-            "joystiq.com",
-            "failblog.org",
-            "pajamasmedia.com",
-            "thepioneerwoman.com",
-            "venturebeat.com",
-            "michellemalkin.com",
-            "thecaucus.blogs.nytimes.com",
-            "slashfilm.com",
-            "gigaom.com",
-            "seekingalpha.com",
-            "wowinsider.com",
-            "gadgets.boingboing.net",
-            "dooce.com",
-            "popwatch.ew.com",
-            "neatorama.com",
-            "crooksandliars.com",
-            "yankodesign.com",
-            "torrentfreak.com",
-            "tuaw.com",
-            "kottke.org",
-            "apartmenttherapy.com",
-            "politico.com",
-            "fivethirtyeight.com",
-            "geekologie.com",
-            "searchengineland.com",
-            "gothamist.com",
-            "techdirt.com",
-            "downloadsquad.com",
-            "chrisbrogan.com",
-            "zenhabits.net",
-            "daringfireball.net",
-            "justjared.buzznet.com",
-            "blog.makezine.com",
-            "visitmix.com",
-            "powerlineblog.com",
-            "metafilter.com",
-            "cinematical.com",
-            "boygeniusreport.com",
-            "codinghorror.com",
-            "firedoglake.com",
-            "radar.oreilly.com",
-            "deadspin.com",
-            "valleywag.gawker.com",
-            "jalopnik.com",
-            "macrumors.com",
-            "makeuseof.com",
-            "stereogum.com",
-            "blog.craftzine.com",
-            "blogs.zdnet.com",
-            "copyblogger.com",
-            "tpmmuckraker.talkingpointsmemo.com",
-            "marginalrevolution.com",
-            "hongkiat.com",
-            "yarnharlot.ca/blog",
-            "freakonomics.blogs.nytimes.com",
-            "alistapart.com",
-            "thesartorialist.blogspot.com",
-            "37signals.com",
-            "radaronline.com",
-            "5minutesformom.com",
-            "jihadwatch.org",
-            "googlesystem.blogspot.com",
-            "orangette.blogspot.com",
-            "angrychicken.typepad.com",
-            "kk.org",
-            "thecoolhunter.net" 
-        ],
-        "media": [
-            "flickr.com",
-            "photobucket.com",
-            "dailymotion.com",
-            "weather.com",
-            "netflix.com",
-            "hulu.com",
-            "vimeo.com",
-            "youtube.com",
-            "deviantart.com",
-            "imageshack.us",
-            "video.yahoo.com",
-            "video.google.com",
-            "pandora.com",
-            "last.fm",
-            "img.4chan.org",
-            "allmusic.com",
-            "pitchfork.com" 
-        ],
-        "shopping": [
-            "amazon.com",
-            "ebay.com",
-            "dpreview.com",
-            "shopzilla.com",
-            "woot.com",
-            "zappos.com",
-            "alibris.com",
-            "kaboodle.com" 
-        ],
-        "social": [
-            "twitter.com",
-            "friendfeed.com",
-            "dopplr.com",
-            "games.yahoo.com",
-            "disney.go.com" 
-        ],
-        "reference": [
-            "wikipedia.com",
-            "imdb.com",
-            "bit.ly",
-            "mahalo.com",
-            "wolframalpha.com",
-            "43things.com",
-            "yelp.com",
-            "biography.com",
-            "catalog.loc.gov",
-            "dictionary.reference.com",
-            "howstuffworks.com",
-            "gutenberg.org",
-            "archive.org",
-            "answers.yahoo.com",
-            "reference.com",
-            "answers.com",
-            "ehow.com",
-            "britannica.com",
-            "about.com",
-            "hubpages.com",
-            "wikihow.com",
-            "loc.gov",
-            "allexperts.com",
-            "wordreference.com",
-            "infoplease.com",
-            "artchive.com",
-            "highbeam.com",
-            "urbandictionary.com",
-            "merriam-webster.com",
-            "instructables.com",
-            "swivel.com",
-            "everything2.com",
-            "wisegeek.com",
-            "twine.com" 
-        ] 
-};
-
-
-function swaptab(domobj){
-    var text = jQuery.trim(jQuery(domobj).text());
-    jQuery('#nav a').removeClass('selected');	         
-    jQuery(domobj).addClass('selected');
-    if (text == 'Share Websites'){
-	jQuery('#profleeditform, #friends').hide();
-	jQuery('#add_sites, #whitelist').show();	      
-    } else if (text == 'Edit Your Profile'){
-	jQuery('#add_sites, #whitelist, #friends').hide();
-	jQuery('#profleeditform').show();	      
-    } else if (text == 'Friends and Followers'){
-	jQuery('#add_sites, #whitelist, #profleeditform').hide();
-	jQuery('#friends').show();	      
-    }
-};
-
-function addGroups(){
-    var groups = [];
-    
-    jQuery('#groups :checkbox:checked').each(function(check){ groups.push(this.name); });
-    if (jQuery('#groupstextinput').val()) {groups.push(jQuery('#groupstextinput').val()); };
-
-    jQuery.post("/add_groups/", {groups: groups}, 
-		function(data){
-		    if (data.code == 200) {
-			jQuery('#groups :checkbox:checked').each(function(check){  this.checked = !this.checked; });
-			jQuery('#groupstextinput').val("");
-			jQuery('#id_tags').val(data.results);
+var Settings = ({
+		    initialize:function(){
+			this.addWhitelist('#urladdul');
+			jQuery('#whitelist').show();
+		    },
+		    // run when the get whitelist function returns
+		    initMenu: function(whitelist){
+			var this_ = this;
+			jQuery('#popularsites ul').hide();
+			jQuery('#popularsites li a').click(
+			    function() {
+				jQuery(this).next().slideToggle('normal');
+			    }
+			);
+			
+			["news","blogs","media","shopping","social","reference"]
+			    .map(function(item){											 
+				     var html = "<a href=\"javascript:addAllPrivacyURLS(\'" +  pageList[item] + "\');\" style=\"font-weight:bold;\"> share all</a>";
+				     var items = pageList[item].sort(function(x, y){ return String.toLowerCase(x) > String.toLowerCase(y); });
+				     items.map(function(domain){
+						   if (whitelist.indexOf(domain) < 0) {
+						       html += this_.drawShareableSite(domain); 
+						   } 
+					       });				    
+				     jQuery("#add_sites #" + item).html(html);				     
+				 });
+		    },		    
+		    setupNewTabPrefs:function(){
+			this.pref = JV3.prefs.getCharPref('which');			
+			if (this.pref == 'personal'){
+			    var mode = JV3.prefs.getCharPref('personalGraphType');
+			    jQuery('#' + mode).addClass('sel');
+			}
+			else {
+			    jQuery('#' + this.pref).addClass('sel');
+			}			
+		    },
+		    addWhitelist:function(div){
+			var this_ = this;
+			jQuery.get("/get_privacy_urls/", {}, function(data){
+				       if (data.code == 200) {
+					       this_.whitelist = data.results.sort(function(x, y){ return String.toLowerCase(x) > String.toLowerCase(y); });
+					       this_.initMenu(this_.whitelist);
+					       this_.addMostSharedSites("#top_logged", this_.whitelist);
+					       jQuery(div).html(this_.whitelist.map(function(item){
+											return this_.drawSharedSite(item);
+										    }).join(''));					       
+				       }  }, "json");
+		    },
+		    addMostSharedSites:function(div, whitelist){
+			var this_ = this;
+			jQuery.get("/get_most_shared_hosts/25/", {}, 
+				   function(data){
+				       if (data.code == 200) {
+					   jQuery(div).html(data.results.map(function(item){ 
+										 if (whitelist.indexOf(item[0]) < 0) {
+										     return this_.drawShareableSite(item[0]);
+										 } 
+									     }).join(''));
+				       }
+				   }, "json");	
+		    },
+		    drawSharedSite:function(domain){
+			return "<li>"
+			    + domain
+			    + "<a onclick=\"deleteSharedSite(\'" + domain + "\'); jQuery(this).parent().hide('slow');\"><img src=\"../../skin/cancel_16.png\"/></a>"
+			    + "</li>";
+		    },
+		    drawShareableSite: function(domain){
+			return "<li>"
+			    + "<a onclick=\"JV3.add_to_whitelist(\'" + domain + "\'); addPrivacyURL(\'" + domain + "\'); jQuery(this).parent().hide('slow');\"><b>share</b></a>&nbsp;&nbsp;&nbsp;"
+			    + domain
+			    +"</li>";			
 		    }
-		    else {
-		    }
-		}, "json");
-}
-
-function initMenu(){
-    jQuery('#popularsites ul').hide();
-    jQuery('#popularsites li a').click(
-	function() {
-	    jQuery(this).next().slideToggle('normal');
-	}
-    );
-    var html = "";
-    var typesArray = ["news","blogs","media","shopping","social","reference"];
-    for (var i = 0; i < typesArray.length; i++){			
-	html = "<a href=\"javascript:addAllPrivacyURLS(\'" + pageList[typesArray[i]] + "\');\" style=\"font-weight:bold;\"> add all</a>";
-	for (var j = 0; j < pageList[typesArray[i]].length; j++){
-	    html += "<li>";
-	    html += "<a href=\"javascript:addPrivacyURL(\'" + pageList[typesArray[i]][j] + "\');\">" + pageList[typesArray[i]][j] + "</a>";
-	    html += "</li>";			
-	}
-	jQuery("#" + typesArray[i]).html(html);
-    }
-};
-function addPrivacyURL(url){
-    var urlEncoded = encodeURIComponent(url);
-    jQuery("<li>" + url + "<a href=\"/delete_privacy_url/?input=" + url + "\"><img src=\"/lib/img/cancel_16.png\"/></a>" + "</li>").prependTo('#urladdul');
-    jQuery.get("/add_privacy_url/", {input:url}); 
-};
-
-function addAllPrivacyURLS(urls){
-    var urlsArray = urls.split(",");
-    for (var i = 0; i < urlsArray.length; i++){
-	jQuery("<li>" + urlsArray[i] + "<a href=\"/delete_privacy_url/?input=" + urlsArray[i] + "\"><img src=\"/lib/img/cancel_16.png\"/></a>" + "</li>").prependTo('#urladdul');		
-    }
-    jQuery.get("/add_privacy_url/", {input:urls}); 
-};
-
-
-// this code is so bad sorry
-jQuery(document).ready(
-    function(){
-	//jQuery('#header').width(window.innerWidth - 15);
-
-	jQuery('#whitelist').show();
-	initMenu();
-	// no params :(
-	jQuery.get("/get_privacy_urls/", {}, 
-		   function(data){
-		       if (data.code == 200) {
-			   var html = "";
-			   for (var i = 0; i < data.results.length; i++) {
-			       html += "<li>";
-			       html += data.results[i];
-			       html += "<a href=\"/delete_privacy_url/?input=" + data.results[i] + "\"><img src=\"/lib/img/cancel_16.png\"/></a>";
-			       html += "</li>";
-			   }
-			   jQuery("#urladdul").html(html);
-		       }
-		       else {
-		       }
-		   }, "json");
-
-	jQuery.get("/get_most_shared_hosts/25/", {}, 
-		   function(data){
-		       if (data.code == 200) {
-			   var html = "";
-			   for (var i = 0; i < data.results.length; i++) {
-			       html += "<li>";
-			       html += "<a href=\"javascript:addPrivacyURL(\'" + data.results[i][0] + "\');\">" + data.results[i][0] + "</a>";
-			       html += "</li>";			
-			   }
-			   jQuery("#top_logged").html(html);
-		       }
-		       else {
-		       }
-		   }, "json");
-
-	self.addURL = function(){
-	    var input = jQuery("#urladdtextinput").val();
-	    jQuery.ajax({
-			    type:"GET",
-			    url:"../add_privacy_url/",
-			    dataType:"json",
-			    data:{input:input},
-			    success:function(data){
-				if (data.host && data.host.length > 0) {
-				    jQuery("<li>" + data.host + "<a href=\"/delete_privacy_url/?input=" + data.host + "\"><img src=\"/lib/img/cancel_16.png\"/></a>" + "</li>").prependTo('#urladdul');
-				}
-			    },
-			    error:function() {}
-			});
-	};
-	jQuery('#groupstextinput').val("");
-    });
+		});
+jQuery(document).ready(function(){  Settings.initialize();   });
