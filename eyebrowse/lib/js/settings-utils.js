74a0700c793e4fbe2e86696e6e72b73bc81bc002
zamiang <zamiang@f708bf84-5827-0410-8242-ae4f0cc3d000>
2010-03-19 06:04:58 +0000

    addings utils file for settings page

---
+++ eyebrowse/lib/js/settings-utils.js
@@ -0,0 +1,316 @@
+var pageList = {
+        "news": [
+            "nytimes.com",
+            "abcnews.go.com",
+            "alarabiya.net",
+            "news.aol.com",
+            "hosted.ap.org",
+            "news.bbc.co.uk",
+            "cbc.ca",
+            "cbsnews.com",
+            "csmonitor.com",
+            "cnn.com",
+            "spiegel.de",
+            "ctv.ca/news",
+            "economist.com",
+            "euronews.net",
+            "foxnews.com",
+            "france24.com",
+            "news.google.com",
+            "guardian.co.uk",
+            "iht.com",
+            "mcclatchydc.com",
+            "msnbc.msn.com",
+            "news.com",
+            "npr.org",
+            "oneworld.net",
+            "latimes.com",
+            "pbs.org",
+            "radionetherlands.nl",
+            "reuters.com",
+            "rte.ie",
+            "news.sky.com",
+            "timesonline.co.uk",
+            "topix.com",
+            "un.org",
+            "usatoday.com",
+            "voanews.com",
+            "wsj.com",
+            "washingtonpost.com",
+            "en.wikinews.org",
+            "wnnetwork.com",
+            "news.yahoo.com",
+            "news.google.com",
+            "espn.go.com",
+            "msn.foxsports.com",
+            "huffingtonpost.com",
+            "drudgereport.com",
+            "foxnews.com",
+            "ign.com",
+            "gamefaqs.com",
+            "digg.com",
+            "engadget.com",
+            "techmeme.com",
+            "wired.com",
+            "slashdot.org",
+            "cnet.com",
+            "microsoft.com",
+            "apple.com",
+            "time.com",
+            "buzz.yahoo.com",
+            "reddit.com",
+            "delicious.com",
+            "diigo.com",
+            "newsvine.com",
+            "fark.com",
+            "propeller.com",
+            "mixx.com",
+            "stumbleupon.com" 
+        ],
+        "blogs": [
+            "bloglines.com",
+            "blogger.com",
+            "wordpress.com",
+            "typepad.com",
+            "livejournal.com",
+            "woostercollective.com",
+            "boingboing.net",
+            "technorati.com",
+            "techcrunch.com",
+            "mashable.com",
+            "ffffound.com",
+            "booooooom.com",
+            "butdoesitfloat.com",
+            "gizmodo.com",
+            "googleblog.blogspot.com",
+            "lifehacker.com",
+            "arstechnica.com",
+            "tmz.com",
+            "readwriteweb.com",
+            "gawker.com",
+            "smashingmagazine.com",
+            "sethgodin.typepad.com",
+            "whitehouse.gov/blog",
+            "kotaku.com",
+            "thedailybeast.com",
+            "dailykos.com",
+            "andrewsullivan.theatlantic.com",
+            "consumerist.com",
+            "treehugger.com",
+            "corner.nationalreview.com",
+            "crunchgear.com",
+            "bits.blogs.nytimes.com",
+            "thinkprogress.org",
+            "icanhascheezburger.com",
+            "gmailblog.blogspot.com",
+            "perezhilton.com",
+            "blogs.abcnews.com",
+            "politicalticker.blogs.cnn.com",
+            "autoblog.com",
+            "joystiq.com",
+            "failblog.org",
+            "pajamasmedia.com",
+            "thepioneerwoman.com",
+            "venturebeat.com",
+            "michellemalkin.com",
+            "thecaucus.blogs.nytimes.com",
+            "slashfilm.com",
+            "gigaom.com",
+            "seekingalpha.com",
+            "wowinsider.com",
+            "gadgets.boingboing.net",
+            "dooce.com",
+            "popwatch.ew.com",
+            "neatorama.com",
+            "crooksandliars.com",
+            "yankodesign.com",
+            "torrentfreak.com",
+            "tuaw.com",
+            "kottke.org",
+            "apartmenttherapy.com",
+            "politico.com",
+            "fivethirtyeight.com",
+            "geekologie.com",
+            "searchengineland.com",
+            "gothamist.com",
+            "techdirt.com",
+            "downloadsquad.com",
+            "chrisbrogan.com",
+            "zenhabits.net",
+            "daringfireball.net",
+            "justjared.buzznet.com",
+            "blog.makezine.com",
+            "visitmix.com",
+            "powerlineblog.com",
+            "metafilter.com",
+            "cinematical.com",
+            "boygeniusreport.com",
+            "codinghorror.com",
+            "firedoglake.com",
+            "radar.oreilly.com",
+            "deadspin.com",
+            "valleywag.gawker.com",
+            "jalopnik.com",
+            "macrumors.com",
+            "makeuseof.com",
+            "stereogum.com",
+            "blog.craftzine.com",
+            "blogs.zdnet.com",
+            "copyblogger.com",
+            "tpmmuckraker.talkingpointsmemo.com",
+            "marginalrevolution.com",
+            "hongkiat.com",
+            "yarnharlot.ca/blog",
+            "freakonomics.blogs.nytimes.com",
+            "alistapart.com",
+            "thesartorialist.blogspot.com",
+            "37signals.com",
+            "radaronline.com",
+            "5minutesformom.com",
+            "jihadwatch.org",
+            "googlesystem.blogspot.com",
+            "orangette.blogspot.com",
+            "angrychicken.typepad.com",
+            "kk.org",
+            "thecoolhunter.net" 
+        ],
+        "media": [
+            "flickr.com",
+            "photobucket.com",
+            "dailymotion.com",
+            "weather.com",
+            "netflix.com",
+            "hulu.com",
+            "vimeo.com",
+            "youtube.com",
+            "deviantart.com",
+            "imageshack.us",
+            "video.yahoo.com",
+            "video.google.com",
+            "pandora.com",
+            "last.fm",
+            "img.4chan.org",
+            "allmusic.com",
+            "pitchfork.com" 
+        ],
+        "shopping": [
+            "amazon.com",
+            "ebay.com",
+            "dpreview.com",
+            "shopzilla.com",
+            "woot.com",
+            "zappos.com",
+            "alibris.com",
+            "kaboodle.com" 
+        ],
+        "social": [
+            "twitter.com",
+            "friendfeed.com",
+            "dopplr.com",
+            "games.yahoo.com",
+            "disney.go.com" 
+        ],
+        "reference": [
+            "wikipedia.com",
+            "imdb.com",
+            "bit.ly",
+            "mahalo.com",
+            "wolframalpha.com",
+            "43things.com",
+            "yelp.com",
+            "biography.com",
+            "catalog.loc.gov",
+            "dictionary.reference.com",
+            "howstuffworks.com",
+            "gutenberg.org",
+            "archive.org",
+            "answers.yahoo.com",
+            "reference.com",
+            "answers.com",
+            "ehow.com",
+            "britannica.com",
+            "about.com",
+            "hubpages.com",
+            "wikihow.com",
+            "loc.gov",
+            "allexperts.com",
+            "wordreference.com",
+            "infoplease.com",
+            "artchive.com",
+            "highbeam.com",
+            "urbandictionary.com",
+            "merriam-webster.com",
+            "instructables.com",
+            "swivel.com",
+            "everything2.com",
+            "wisegeek.com",
+            "twine.com" 
+        ] 
+};
+
+var addURL =  function(){
+    var input = jQuery("#urladdtextinput").val();
+    jQuery.ajax({
+		    type:"GET",
+		    url:"../add_privacy_url/",
+		    dataType:"json",
+		    data:{input:input},
+		    success:function(data){
+			if (data.host && data.host.length > 0) {
+			    jQuery("<li>" + data.host + "<a href=\"/delete_privacy_url/?input=" + data.host + "\"><img src=\"../../skin/cancel_16.png\"/></a>" + "</li>").prependTo('#urladdul');
+			}
+		    },
+		    error:function() {}
+		});
+};
+
+function swaptab(domobj){
+    var text = jQuery.trim(jQuery(domobj).text());
+    jQuery('#nav a').removeClass('selected');	         
+    jQuery(domobj).addClass('selected');
+    if (text == 'Share Websites'){
+	jQuery('#profleeditform, #friends').hide();
+	jQuery('#add_sites, #whitelist').show();	      
+    } else if (text == 'Edit Your Profile'){
+	jQuery('#add_sites, #whitelist, #friends').hide();
+	jQuery('#profleeditform').show();	      
+    } else if (text == 'Friends and Followers'){
+	jQuery('#add_sites, #whitelist, #profleeditform').hide();
+	jQuery('#friends').show();	      
+    }
+};
+
+
+function addGroups(){
+    var groups = [];
+    
+    jQuery('#groups :checkbox:checked').each(function(check){ groups.push(this.name); });
+    if (jQuery('#groupstextinput').val()) {groups.push(jQuery('#groupstextinput').val()); };
+
+    jQuery.post("/add_groups/", {groups: groups}, 
+		function(data){
+		    if (data.code == 200) {
+			jQuery('#groups :checkbox:checked').each(function(check){  this.checked = !this.checked; });
+			jQuery('#groupstextinput').val("");
+			jQuery('#id_tags').val(data.results);
+		    }
+		    else {
+		    }
+		}, "json");
+}
+
+function addPrivacyURL(url){
+    jQuery("<li>" + url + "<a href=\"http://eyebrowse.csail.mit.edu/delete_privacy_url/?input=" + url + "\"><img src=\"../../skin/cancel_16.png\"/></a>" + "</li>").prependTo('#urladdul');
+    jQuery.get("http://eyebrowse.csail.mit.edu/add_privacy_url/", {input:url}); 
+};
+
+function addAllPrivacyURLS(urls){
+    var urlsArray = urls.split(",");
+    for (var i = 0; i < urlsArray.length; i++){
+	jQuery("<li>" + urlsArray[i] + "<a href=\"http://eyebrowse.csail.mit.edu/delete_privacy_url/?input=" + urlsArray[i] + "\"><img src=\"../../skin/cancel_16.png\"/></a>" + "</li>").prependTo('#urladdul');		
+    }
+    jQuery.get("http://eyebrowse.csail.mit.edu/add_privacy_url/", {input:urls}); 
+};
+
+var deleteSharedSite = function(url) { jQuery.get("http://eyebrowse.csail.mit.edu/delete_privacy_url/", {input:url}); };
+var unfollowUser = function(user){ jQuery.get("http://eyebrowse.csail.mit.edu/friend/unfollow/", {friend:user}); };
