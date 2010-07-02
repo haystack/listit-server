c7f50b821d6acba4215e66260566f7d762073c8b
emax <emax@f708bf84-5827-0410-8242-ae4f0cc3d000>
2010-07-02 19:14:58 +0000

    some improvements

--- jv3/study/ca_plot.py
+++ jv3/study/ca_plot.py
@@ -10,7 +10,7 @@ r = ro.r
 em = User.objects.filter(email="emax@csail.mit.edu")[0]
 c = lambda vv : apply(r.c,[float(v) for v in vv])
 
-def hist(data,breaks="auto",auto_nbins_top=50,auto_nbins_skip=1,filename="/var/www/hist.out.png",title="histogram of something",xlabel="x",ylabel="counts",width=1280,height=1024):
+def hist(data,breaks="auto",auto_nbins_top=50,auto_nbins_skip=1,filename="/var/www/hist.out.png",title="histogram of something",xlabel="x",ylabel="counts",width=1280,height=1024,devoff=True):
     # data should be raw [1,2,1,2,3,2,...]
     # 
     r.png(file=filename,width=width,height=height)
@@ -24,7 +24,7 @@ def hist(data,breaks="auto",auto_nbins_top=50,auto_nbins_skip=1,filename="/var/w
     print breaks
         
     histout = r.hist(c(data),breaks=breaks,plot=True,main=title,xlab=xlabel,ylab=ylabel)
-    r('dev.off()')
+    if devoff: r('dev.off()')
     return histout
 
 make_axis_labels = lambda breaks: [ "%g-%g" % (breaks[i],breaks[i+1]) for i in range(len(breaks)-1)]
@@ -52,7 +52,7 @@ def hist2(data,breaks="Scott",auto_nbins_top=50,auto_nbins_skip=1,
 
 def loghist(data,breaks="Scott",auto_nbins_top=50,auto_nbins_skip=1,
             breaklabels=None,
-            filename="/var/www/hist.out.png",title="histogram of something",xlabel="x",ylabel="counts",width=1280,height=1024,**kwargs):
+            filename="/var/www/hist.out.png",title="histogram of something",xlabel="x",ylabel="counts",width=1280,height=1024,devoff=True,**kwargs):
 
     r.png(file=filename,width=width,height=height)
     if type(breaks) == list:
@@ -66,16 +66,16 @@ def loghist(data,breaks="Scott",auto_nbins_top=50,auto_nbins_skip=1,
         breaklabels = make_axis_labels([x for x in histout[0]][0:])
     kwargs.update( {"names.arg":breaklabels} )
     r.barplot( c([log(x+1)/log(10) for x in histout[1]] ),plot=True,main=title,**kwargs) 
-    r('dev.off()')
+    if devoff: r('dev.off()')
     return histout
 
 
-def scatter(data,filename="/var/www/scatter.out.png",title="histogram of something",xlabel="x",ylabel="y",width=1280,height=1024,**kwargs):
+def scatter(data,filename="/var/www/scatter.out.png",title="histogram of something",xlabel="x",ylabel="y",width=1280,height=1024,devoff=True,**kwargs):
     ## data should be [ (x,y) .. ]
     # example: cap.scatter( [(n['jid'],n['deleted']) for n in renotes[0]],title="deleted versus not deleted", xlabel="not deleted/deleted", ylabel="# of notes (out of 2500)")
     r.png(file=filename,width=width,height=height)
     out = r.plot(c([float(d[0]) for d in data]),c([float(d[1]) for d in data]),main=title,xlab=xlabel,ylab=ylabel,**kwargs)
-    r('dev.off()')
+    if devoff: r('dev.off()')
     return out
 
 def bar(data,filename="/var/www/bar.out.png",title="barplot of something",xlabel="x",ylabel="y",width=1280,height=1024):
