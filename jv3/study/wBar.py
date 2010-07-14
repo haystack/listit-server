4a7e21e4ef8f725db44fee265846be0107175dd2
wstyke <wstyke@f708bf84-5827-0410-8242-ae4f0cc3d000>
2010-07-14 05:43:13 +0000

    Examining width of created bars - # note versions (not actionlog versions)  - in wBar.py

--- jv3/study/wBar.py
+++ jv3/study/wBar.py
@@ -12,6 +12,7 @@ def sBar(filename, user, title='title'):
     nOldEdit, nNewEdit = [[] for n in range(ROW_GROUPS)], [[] for n in range(ROW_GROUPS)]
     createdSize = [[0,0] for i in range(ROW_GROUPS)] ## [# notes, # lines] for each day of week
     editedSize = [[0,0] for i in range(ROW_GROUPS)]
+    noteD = {}
     for log in allLogs:
         noteArr = notes.filter(jid=log.noteid)
         if len(noteArr) < 1:  ## Processing logs for which we still
@@ -26,38 +27,35 @@ def sBar(filename, user, title='title'):
         actInPastWk = math.fabs(actTD.days) <= 6 ## Both .created and .when  happened within (current day + 6 previous days)
         if (log.action == 'note-add'):      ## Record Add
             data[wksToIndex(birthDay, actDay*GROUP_TYPES+0)] += 1
-            if len(note.contents) > 0 and len(note.contents) < 1000:
+            if len(note.contents) > 0 and len(note.contents) < 10000:
                 createdSize[actDay][0] += 1   ## Increment: ave note size,
                 ##increment = 0 if note.deleted else 50
-                increment = numLines(log.noteText) if log and log.noteText and (numLines(log.noteText) < 1000) else 0
+                ##increment = numLines(log.noteText) if log and log.noteText and (numLines(log.noteText) < 1000) else 0
+                increment = note.version*10
                 createdSize[actDay][1] += int(increment)
         elif (log.action == 'note-save'):   ## Record Save: Split (edit on day of note.created vs not)
             addVal = 1 if actInPastWk else 2
             if (actInPastWk and log.noteid in nNewEdit[actDay]) or (not actInPastWk and log.noteid in nOldEdit[actDay]):
                 continue  ## We've already recorded this note for it's time-frame
             data[wksToIndex(birthDay, actDay*GROUP_TYPES + addVal)] += 1
-            if actInPastWk:
-                nNewEdit[actDay].append(log.noteid)
-            else:
-                nOldEdit[actDay].append(log.noteid)
+            nNewEdit[actDay].append(log.noteid)  if actInPastWk else nOldEdit[actDay].append(log.noteid)  ## Register Log
         elif (log.action == 'note-delete'): ## Record Death
             addVal = 3 if actInPastWk else 4
             data[wksToIndex(birthDay, actDay*GROUP_TYPES + addVal)] += 1
             pass
     r.png(file = '/var/listit/www-ssl/_studywolfe/' + filename + '.png', w=1000,h=500)
     dayNames = ["Mon","Tues","Wed","Thur","Fri","Sat","Sun"]
-    axisNames = []
-    [axisNames.extend([dayNames[i],"","","",""]) for i in range(ROW_GROUPS)]
     colors = r.c("red", 'orange', 'yellow', 'green', 'blue', 'grey', 'brown')
     title = "#Notes:#Logs:Email:ID -- " + str(notes.count()) + ":" + str(allLogs.count()) + ":" + user.email + ":" + str(user.id)
     aveWidth = int(float(sum([elt[1] for elt in createdSize]))/float(sum([elt[0] for elt in createdSize])))
     widths = []
     [widths.extend([aveSize(elt[1],elt[0]), aveWidth, aveWidth, aveWidth, aveWidth]) for elt in createdSize]
+    axisNames = []
+    [axisNames.extend([str(widths[i*5]),"",str(dayNames[i]),"",""]) for i in range(ROW_GROUPS)]
     r.barplot(data, main=title,ylab='# Action Logs',beside=False, col=colors, space=r.c(3,1,0.1,1,.1), names=axisNames, width=c(widths))
     devoff()
 
 ## How to tell when a note is one 'phrase' repeated over and over? , most of 2.4k notes over 1000 chars are like this!!!!!!!!!!!
-
 sBar('wt00',gv,"GV's Note Action Periodicities")
 
 def test_wBar(s):
@@ -83,3 +81,15 @@ def makeACUBarPlots():
     print "Users processed: ", str(i) , " out of: ", str(len(u))
     print "Finish time: ", time.gmtime()
 
+
+## Get a note dictionary with each note's id as a key, and each type of action as sub-dict-keys,
+## so you can easily count up "version" of a note
+def getNoteDict(logs,notes):
+    noteD = {}
+    for log in logs:
+        if log.noteid in noteD:
+            if log.action in noteD[log.noteid]:
+                pass
+            pass
+        pass
+    pass
