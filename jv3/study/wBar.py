427a89b67e609c3e662ce0e9eb84bdd6c53afcae
wstyke <wstyke@f708bf84-5827-0410-8242-ae4f0cc3d000>
2010-07-14 08:04:01 +0000

    added one lambda fn for easier handling

--- jv3/study/wBar.py
+++ jv3/study/wBar.py
@@ -3,12 +3,13 @@ from datetime import datetime as dd  # Stacked Bar Graph Function Helpers
 def sBar(filename, user, title='title'):
     numLines = lambda txt: len(txt.splitlines())-txt.splitlines().count('')
     aveSize = lambda a,b: int(float(a)/float(b)) if b != 0 else 0  ## a=quantity of something per how many b elts, if no b, return 0
+    wksToIndex = lambda rowWeek, colWeek : rowWeek + (colWeek)*COL_SEGMENTS
+    msecToDate = lambda msec : datetime.datetime.fromtimestamp(float(msec)/1000.0)
     DAY_IN_MS = 1000*60*60*24
     COL_SEGMENTS, ROW_GROUPS, GROUP_TYPES = 7,7,5 ## add, edit,edit, del,del
     notes = user.note_owner.all()
     allLogs = ActivityLog.objects.filter(owner=user, action__in=['note-add','note-save','note-delete'])
     data = r.matrix(0,nrow=COL_SEGMENTS, ncol=ROW_GROUPS*GROUP_TYPES)
-    wksToIndex = lambda rowWeek, colWeek : rowWeek + (colWeek)*COL_SEGMENTS
     nOldEdit, nNewEdit = [[] for n in range(ROW_GROUPS)], [[] for n in range(ROW_GROUPS)]
     createdSize = [[0,0] for i in range(ROW_GROUPS)] ## [# notes, # lines] for each day of week
     editedSize = [[0,0] for i in range(ROW_GROUPS)]
@@ -18,8 +19,7 @@ def sBar(filename, user, title='title'):
         if len(noteArr) < 1:  ## Processing logs for which we still
             continue          ## have the note (deleted or not)
         note = noteArr[0]
-        actDate = dd.fromtimestamp(float(log.when)/1000.0)
-        birthDate = dd.fromtimestamp(float(note.created)/1000.0)
+        actDate, birthDate = msecToDate(log.when), msecToDate(note.created)
         actDay, birthDay = actDate.weekday(), birthDate.weekday()
         firstRecord, lastTime = min(actDate, birthDate ), max(actDate,birthDate)
         startOfDay = dd(lastTime.year, lastTime.month, lastTime.day)
@@ -51,7 +51,7 @@ def sBar(filename, user, title='title'):
     widths = []
     [widths.extend([aveSize(elt[1],elt[0]), aveWidth, aveWidth, aveWidth, aveWidth]) for elt in createdSize]
     axisNames = []
-    [axisNames.extend([str(widths[i*5]),"",str(dayNames[i]),"",""]) for i in range(ROW_GROUPS)]
+    [axisNames.extend([str(widths[i*5]),"","",str(dayNames[i]),""]) for i in range(ROW_GROUPS)]
     r.barplot(data, main=title,ylab='# Action Logs',beside=False, col=colors, space=r.c(3,1,0.1,1,.1), names=axisNames, width=c(widths))
     devoff()
 
