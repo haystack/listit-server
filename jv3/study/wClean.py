cd020f2672495d09e6640c23171442215b4a64da
emax <emax@f708bf84-5827-0410-8242-ae4f0cc3d000>
2010-07-27 00:19:49 +0000

    updated wclean for edit cleaner

--- jv3/study/wClean.py
+++ jv3/study/wClean.py
@@ -2,6 +2,7 @@
 ## ie. Tutorial notes, notes experiencing sync problems (repeated texts), etc
 
 from jv3.utils import is_tutorial_note
+from jv3.models import *
 
 def clean_tutorial_notes():
     i = 0
@@ -18,31 +19,41 @@ def clean_noteorder():
     pass
 
 def rid_of_edits_that_dont_change_note():
-    kill_list_alids = []
     from django.contrib.auth.models import User
+
+    kill_list_alids = []
+    errors = []
     for u in User.objects.all():
         user_kill = []
-        edits_per_jid= dict([(jid,[(alid,text)])  for jid,alid,text in u.activitylog_set.filter(action="note-add").values_list('noteid','id','noteText')])
-        # we have to believe they're consecutive
-        alog_edits = u.activitylog_set.filter(action='note-edit').order_by('when').values_list('id','noteid','noteText')
-        for alid,jid,noteText in alog_edits:
-            v = edits_per_jid.get(jid,[])
-            v.append( (alid,noteText) )
-            edits_per_jid[jid] = v
-            
-        for jid,editpairs in edits_per_jid.iteritems():
-            noteText = editpairs[0][1]
-            for epair in editpairs[1:]:
-                alid,newText = epair
-                if noteText == newText:   # nothing's changed,kill
-                    user_kill.append(alid)
-                else:
-                    noteText = newText
+        try :
+            edits_per_jid= dict([(jid,[(alid,text)])  for jid,alid,text in u.activitylog_set.filter(action="note-add").values_list('noteid','id','noteText')])
+            # we have to believe they're consecutive
+            alog_edits = u.activitylog_set.filter(action='note-edit').order_by('when').values_list('id','noteid','noteText')
+            for alid,jid,noteText in alog_edits:
+                v = edits_per_jid.get(jid,[])
+                v.append( (alid,noteText) )
+                edits_per_jid[jid] = v
+                
+            for jid,editpairs in edits_per_jid.iteritems():
+                noteText = editpairs[0][1]
+                for epair in editpairs[1:]:
+                    alid,newText = epair
+                    if noteText == newText:   # nothing's changed,kill
+                        user_kill.append(alid)
+                    else:
+                        noteText = newText
+                    pass
                 pass
-            pass
-        print "edits to kill for %s : %d " % (u.email,len(user_kill))
-        kill_list_alids = kill_list_alids + user_kill
+            print "edits to kill for %s : %d " % (u.email,len(user_kill))
+            kill_list_alids = kill_list_alids + user_kill
+        except :
+            import traceback,sys            
+            errors.append(u)
+            print sys.exc_info()
+            traceback.print_tb(sys.exc_info()[2])
     # ActivityLog.objects.filter(id__in=kill_list_alids).delete()
+
+    print "ERRORS %d : %s",(len(errors),repr(errors))
     return ActivityLog.objects.filter(id__in=kill_list_alids)
                 
         
