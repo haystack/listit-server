d7e234a828fb02b8120d26b8acaed0e2362e78e0
emax <emax@f708bf84-5827-0410-8242-ae4f0cc3d000>
2010-08-02 00:11:05 +0000

    updated to have new color func

--- jv3/study/wFunc.py
+++ jv3/study/wFunc.py
@@ -48,7 +48,7 @@ def col_of_edit(initText,type,when=0):
     print 'invalid type given to col_of_edit in wFunc.py'
 
 ## Given ONE user's notes, plot note attribute (created) vs activitylog attribute (when)
-def mmmPlot(filename, notes,  title='title', make_filename=''):
+def mmmPlot(filename, notes,  title='title'):
   firstBirth = 1217622560992.0
   ## Meta-data for title
   allLogs = ActivityLog.objects.filter(owner=notes[0].owner, action__in=['note-add','note-delete'])
@@ -133,3 +133,87 @@ def makeACUPlots():
     print "Users processed: ", str(i) , " out of: ", str(len(u))
     print "Finish time: ", time.gmtime()
     finishTime = time.time()
+
+
+
+## Given ONE user's notes, plot note attribute (created) vs activitylog attribute (when)
+
+msecToWeek = lambda x : x * (1.0/(1000.0*60*60*24*7))
+
+def one_or_no_url_redblue(note):
+  from thesis_figures import n2vals
+  note = n2vals(note)
+  urls = ca.note_urls(note)
+  if type(urls) == dict:
+    return 'blue' if urls['note_urls'] > 0 else 'red'
+  return 'blue' if urls[1] > 0 else 'red'
+    
+def lifelineFlatCollapsedCompareColor(filename, notes, title='title', color_function=one_or_no_url_redblue, YMAG=0.5):
+  ## Meta-data for title
+  allLogs = ActivityLog.objects.filter(owner=notes[0].owner, action__in=['note-add','note-delete'])
+  firstBirth = float(min([x[0] for x in notes.values_list('created')]))
+  title = "%s -- %s %s  " % (title,str(notes.count()),notes[0].owner.email)
+  ## Meta-data for points
+  points = {'note-add':r.c(), 'note-delete':r.c()} ## Shortened to just the two, since note-edit added later
+  births, deaths = {}, {}, 
+  today = time.time()*1000.0
+  print "saving to %s " % cap.make_filename(filename)
+  r.png(file=cap.make_filename(filename), w=6400,h=3200) ## 3200x1600, 9600x4800, 12.8x6.4
+  colors=dict([(n.jid,color_function(n)) for n in notes])  
+
+  # order notes by creation
+  jids = [id[0] for id in notes.order_by("created").values_list('jid')]
+  jid2idx = dict([(jids[i],i) for i in xrange(len(jids))])
+  
+  for log in allLogs:
+     noteArr = notes.filter(jid=log.noteid)
+     if len(noteArr) < 1:
+        continue
+     note = noteArr[0]
+     births[note.jid] = jid2idx[note.jid]
+     if not note.deleted and note.jid not in deaths:
+        deaths[note.jid] = today
+        pass
+     if (log.action == 'note-delete'):
+        deaths[note.jid] = float(log.when-note.created)
+     points[log.action] = r.rbind(points[log.action],c([jid2idx[note.jid],float(log.when-note.created)]))
+     pass
+
+  # compute the edits, compile the colors
+  edits_ = ca.note_edits_for_user(notes[0].owner)
+  points['note-edit'] = r.c()
+  edit_dir, edit_delta, colors_r = r.c(), r.c(), r.c(),
+  ymax = 0
+  for n in notes:  ## wCom: not all notes eval'd here may have note-add events !!
+    colors_r = r.c(colors_r,colors[n.jid])
+    if n.jid in edits_:
+      for edit_action in edits_[n.jid]:
+        ## Add big icon
+        ymax = max(ymax,float(edit_action['when']-n.created))
+        points['note-edit'] = r.rbind(points['note-edit'],c([jid2idx[n.jid], float(edit_action['when']-n.created)]))
+        edit_dir = r.c(edit_dir, pch_of_delta(edit_action['initial'],edit_action['final']))
+        edit_delta = 3 # r.c(edit_delta, abs(3 + 1*(len(edit_action['initial']) - len(edit_action['final']))/1000.0))
+        # ## Add small icon
+        #         points['note-edit'] = r.rbind(points['note-edit'],c([jid2idx[n.jid], float(edit_action['when'])]))
+        #         edit_dir = r.c(edit_dir, pch_of_innerEdit(edit_action['initial'],edit_action['final']))
+        #         edit_delta = r.c(edit_delta, 7)#abs(10 + 10*(len(edit_action['initial']) - len(edit_action['final']))/1000.0))
+        #         edit_col = r.c(edit_col, col_of_edit(edit_action['initial'], 'inner', edit_action['when']))
+        
+  print points['note-edit']
+  xl,yl="Created Date", "Action Date"
+  r.plot(points['note-add'], cex=2.0,col=colors_r, pch='o',xlab=xl,ylab=yl, main=title, xlim=r.c(0, notes.count()),ylim=r.c(0, YMAG*ymax), axes=False)
+  r.points(points['note-edit'], col=colors_r, pch=edit_dir,  cex=edit_delta)
+  r.points(points['note-delete'], cex=2.0,col = colors_r, pch='x')
+  yWeeks = [int(y) for y in range(int(msecToWeek(firstBirth)), int(msecToWeek(time.time()*1000)), 1)]
+  r.axis(2, at=c([float(y*7*24*60*60*1000.0) for y in yWeeks]), labels=c([int(x)-2012 for x in yWeeks]), tck=1)
+
+  for x in births.keys():
+     if x in deaths:
+       stillalive = (today-deaths[x] < 0.001)
+       color = colors[x] # ('green' if stillalive else 'black')
+       r.lines(c([float(births[x])]*2),c([float(births[x]),float(deaths[x]-births[x])]), col=color)
+       pass
+  devoff()
+
+
+
