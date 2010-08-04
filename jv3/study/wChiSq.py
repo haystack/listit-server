3bb20a3474d18e84387044c7e51fbd005bb9f751
wstyke <wstyke@f708bf84-5827-0410-8242-ae4f0cc3d000>
2010-08-04 00:29:55 +0000

    added chisq test for sameday vs diff-day for all users

--- jv3/study/wChiSq.py
+++ jv3/study/wChiSq.py
@@ -105,3 +105,33 @@ def chisq_edits_for_users(users):
         pass
     result = r('chisq.test')(chiMatrix)
     return (result, chiMatrix)
+
+
+
+def chisq_edits_for_users_2(users):
+    chiMatrix = r.matrix(c([0 for i in range(14)]), ncol=2,nrow=7)
+    for user in users:
+        notes = Note.objects.filter(owner=user)
+        edits_ = ca.note_edits_for_user(user)
+        for n in notes:
+            if n.jid in edits_:
+                createdDOW = wUtil.msecToDate(n.created).weekday()
+                for edit_action in edits_[n.jid]:
+                    timeDelta = edit_action['when'] - n.created
+                    if timeDelta > 1000*3600*24*7:
+                    ## For each edit of this note, find editDOW and increment spot in matrix
+                        editDOW = wUtil.msecToDate(edit_action['when']).weekday()
+                        if createdDOW == editDOW:
+                            chiMatrix[createdDOW] += 1
+                        else:
+                            chiMatrix[createdDOW+7] += 1
+                            pass
+                        pass
+                    pass
+                pass
+            pass
+        pass
+    result = r('chisq.test')(chiMatrix)
+    return (result, chiMatrix)
+
+
