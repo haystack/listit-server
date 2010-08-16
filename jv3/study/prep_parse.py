e2404c28b9d1fd4d05129506dd14e4dd69eeacab
emax <emax@f708bf84-5827-0410-8242-ae4f0cc3d000>
2010-08-16 02:20:07 +0000

    prep parser

---
+++ jv3/study/prep_parse.py
@@ -0,0 +1,42 @@
+
+# tasty parser based on prepositions 
+# by emax
+
+import re
+
+def find_up_to_N_words_out(s,N=3):
+    starts = 0
+    word = re.compile('\w+(\s|$)')
+    for i in range(N):
+        next = word.search(s,starts)
+        if next is None: return len(s)
+        starts = next.end() + 1
+    return starts - 1
+
+special_preps = ["@","at","about","after","before","by","around","during","for","from","in","into","near","on","past","per","since","thru","through","over","until","till","to","under","via","with","w/"]
+special_preps_re  = [ re.compile('(^|\s)%s(\s|$)' % p) for p in special_preps ]
+
+def nonemin(a,b):
+    if a is None: return b
+    if b is None: return a
+    return min(a,b)
+
+def find_next_special_prep_idx(s):
+    starts = [x.start() for x in [pre.search(s) for pre in special_preps_re] if x is not None]
+    if len(starts) == 0: return None
+    return min(starts)
+
+def parse(text):
+    prep_bindings = {}
+    for c in text.lower().strip().split('\n'):
+        for pre in special_preps_re:
+            search = pre.search(c)
+            while search is not None:
+                cpos,keyend = search.start(),search.end()
+                val_end = keyend + 1 + nonemin(find_up_to_N_words_out(c[keyend+1:]), find_next_special_prep_idx(c[keyend+1:]))
+                key = c[cpos:keyend].strip()
+                val = c[keyend:val_end].strip()
+                if len(val) > 0:   prep_bindings[key] = prep_bindings.get(key,[]) + [val]
+                search = pre.search(c,cpos + 1)
+            pass
+    return prep_bindings
