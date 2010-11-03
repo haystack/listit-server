73503c8b7f58e12d6d4192ce8316b712c809d90a
wstyke <wstyke@f708bf84-5827-0410-8242-ae4f0cc3d000>
2010-11-03 16:05:06 +0000

    Adding wMaxEntModel.py -- the start of classifying notes as a category or not a category

---
+++ jv3/study/wMaxEntModel.py
@@ -0,0 +1,63 @@
+## This is for Wolfe's 6.864 class project
+## Design a Maximum Entropy (Log-linear model) to classify notes
+## as belonging or not belonging to a particular class
+## Classes: todo-reminder, reference, ext cog., journal/logging, 
+## personal archival
+
+from django.contrib.auth.models import User
+from jv3.models import *
+from jv3.utils import *
+import jv3.study.content_analysis as ca
+import jv3.study.ca_datetime as cadt
+import jv3.study.ca_sigscroll as cass
+import jv3.study.ca_load as cal
+import jv3.study.ca_plot as cap
+from jv3.study.ca_plot import make_filename
+import jv3.study.ca_search as cas
+import rpy2
+import rpy2.robjects as ro
+from jv3.study.study import *
+from numpy import array
+import random
+
+import jv3.study.note_labels as nl
+import jv3.study.keeping_labels as kl
+import jv3.study.intention as intent
+import ntlk
+
+def getNoteCat():
+    noteData = intent.reada()
+    ## of form: [#, rater name, #, noteid?, notetext, 
+    ## most-likely-category, #- descword ** 5 for each cat]
+    ## noteData[2] = user.id, noteData[3] = note.jid
+    ## Specifically: noteData[4] => note text
+    ## noteData[5] => highest rated category    
+    return noteData
+
+def splitNotes(category):
+    getOwner = lambda x : User.objects.filter(id=x)[0] # turns id into owner
+    noteData = getNoteCat()
+    matches = filter(lambda x:x[5]==category, noteData)
+    nonMatches = filter(lambda x:x[5]!=category, noteData)
+    ## Turn list of category-note data into two list of notes
+    matchNotes = map(lambda x:Note.objects.filter(owner=getOwner(x[2]), jid=x[3])[0], matches)
+    nonMatchNotes = map(lambda x:Note.objects.filter(owner=getOwner(x[2]), jid=x[3])[0], nonMatches)
+    return (matchNotes, nonMatchNotes)
+
+def initMaxEntModel(labels, mapping, weights):
+    encoding = nltk.BinaryMaxentFeatureEncoding(labels, mapping)
+    maxEntModel = nltk.MaxentClassifier(encoding, weights)
+    return maxEntModel
+
+def getStdMapping():
+    ## Returns (mapping, weights)
+    
+
+
+def refClassifier():
+    ## Ref and nonRef thingies
+    refNotes, nonRefNotes = splitNotes('reference')
+    labels = ['reference', 'nonref']
+    ## 
+    mapping, weights = getStdMapping()
+    maxEntModel = initMaxEntModel(labels, mapping, weights)
