2575705c185608095dd2dd4ef118d7d57109d3f6
wstyke <wstyke@f708bf84-5827-0410-8242-ae4f0cc3d000>
2010-11-04 23:48:23 +0000

    Functional, with some features, needs moar featurezfg

--- jv3/study/wMaxEntModel.py
+++ jv3/study/wMaxEntModel.py
@@ -3,6 +3,7 @@
 ## as belonging or not belonging to a particular class
 ## Classes: todo-reminder, reference, ext cog., journal/logging, 
 ## personal archival
+## FEATURE SETS ## http://nltk.googlecode.com/svn/trunk/doc/api/nltk.classify-module.html
 
 from django.contrib.auth.models import User
 from jv3.models import *
@@ -23,7 +24,8 @@ import random
 import jv3.study.note_labels as nl
 import jv3.study.keeping_labels as kl
 import jv3.study.intention as intent
-import ntlk
+import nltk
+import jv3.study.thesis_figures as tfigs
 
 def getNoteCat():
     noteData = intent.reada()
@@ -48,16 +50,6 @@ def initMaxEntModel(encoding, weights):
     maxEntModel = nltk.MaxentClassifier(encoding, weights)
     return maxEntModel
 
-def getStdMapping():
-    ## Returns (mapping, weights)
-    ## how to get mapping?
-    
-    
-    
-    ## Choose weights randomly
-    weights = [0]*10 ## change this to how ever many features there are!
-
-
 def refClassifier():
     ## Ref and nonRef thingies
     refNotes, nonRefNotes = splitNotes('reference')
@@ -67,56 +59,83 @@ def refClassifier():
     encoding = nltk.BinaryMaxentFeatureEncoding(labels, mapping)
     maxEntModel = initMaxEntModel(encoding, weights)
 
+def nvals(note):
+    return tfigs.n2vals(note)
 
+def word_is_verb(word):
+    is_verb = False
+    for qq,pos in nltk.pos_tag(word):
+        if pos in ["VB","VBD","VBG","VBN","VBP","VBZ"]:
+            is_verb = True
+            break
+    return is_verb
 
-## 
-#In [147]: BMFE.train([({'fname0':0,'fname1':1},'ref')])
-#Out[147]: <nltk.classify.maxent.BinaryMaxentFeatureEncoding object at 0x7517110>
+mem, notMem = splitNotes('memory trigger')
 
-#In [149]: BMFE.train([({'fname0':0,'fname1':1},'ref')])
-#Out[149]: <nltk.classify.maxent.BinaryMaxentFeatureEncoding object at 0x7517250>
+mapping = {}
+labels = ['memory trigger', 'not']
 
-#In [151]: BMFE.labels()
-#Out[151]: ['1', '2']
+def addFeature(featureset, fname, fval):
+    global mapping, labels
+    featureset[fname] = fval
+    if (fname, fval, labels[0]) not in mapping:
+        for label in labels:
+            mapping[(fname,fval,label)]=len(mapping)
 
-#In [152]: mapp = {('fname0','fval0','nonref'):0,('fname','fval','ref'):1}
-
-#In [153]: BMFE = nltk.BinaryMaxentFeatureEncoding(['1','2'], mapp)
-
-
-# mapp = {('fname0','fval0','nonref'):'nonref',('fname','fval','ref'):'ref'}
-# BMFE = nltk.BinaryMaxentFeatureEncoding(labels=['ref','nonref'], mapping=mapp)
-# BMFE.labels() --> Out[165]: ['ref', 'nonref']
-# BMFE.train(train_toks=[({'fname0':'fval0'},"ref")])
-
-# BMFE.labels() --> ['ref', 'nonref']
-# BMFE.length() --> 2
-
-
-## FEATURE SETS ## http://nltk.googlecode.com/svn/trunk/doc/api/nltk.classify-module.html
-## Training Set: List of (featuredict, label) tuples !
-
-## Feature Set: dict mapping from "feature name to feature value"
-
-defaultMapping = {("contains-word",'email',True):0}
-
-def note_features(note):
+def noteFeatures(note):
+    notevals = nvals(note)
     featureset = {}
-    ## Add questions to feature set like:
-    for word in note.contents.split(' '):
-        featureset["contains-word(%s)" % word] = True 
-    ## more!
+    ## Add questions to feature set!
+    words = nltk.word_tokenize(note.contents)
+#    for word in words: ## overfitting?
+        #addFeature(featureset,"contains-word(%s)"%(word), True)
+    ## Note Text Content
+    first_word = words[0]
+    first_word_verb = word_is_verb(first_word)
+    if first_word_verb:
+        addFeature(featureset, "first-word-is-verb", True)
+        #addFeature(featureset, "first-verb-is(%s)"%(first_word), True)
+    verb_count = ca.note_verbs(notevals)['note_verbs']
+    addFeature(featureset, "contains-k-verbs(%s)"%(verb_count), True)
+    note_words = ca.note_words(notevals)['note_words']
+    for i in range(note_words-1,note_words+2):
+        addFeature(featureset, "contains-k-words(%s)"%(max(i, 0)), True)
+    note_lines = ca.note_lines(notevals)['note_lines']
+    addFeature(featureset, "contains-k-lines(%s)"%(note_lines), True)
+    note_urls = ca.note_urls(notevals)['note_urls']
+    if note_urls > 0:
+        addFeature(featureset, "contains->1-urls()", True)
+        addFeature(featureset, "contains-k-urls(%s)"%(note_urls), True)
+    ## 80%
+    numbers = ca.numbers(notevals)['numbers']
+    if numbers > 0:
+        addFeature(featureset, "contains->0-numbers()", True)
+    symDaysofweek = "yes" if (ca.daysofweek(notevals)['daysofweek'] > 0) else "no"
+    addFeature(featureset, "contains-daysofweek(%s)"%(symDaysofweek), True)
     return featureset
 
+def createTokens(notesA, labelA, notesB, labelB):
+    train_toks = [(noteFeatures(note), labelA) for note in notesA]
+    [train_toks.append((noteFeatures(note), labelB)) for note in notesB]
+    return train_toks
 
-## What is mapping?:  dict like:               {(featurename, featurevalue, label):index}
-## ex: featurename = contains-word(foobar)
-## ex: featurevalue  = True
-## ex: label = category or not...
+def createMemTokens():
+    mem, notMem = splitNotes('memory trigger')
+    return createTokens(mem, 'memory trigger', notMem, 'not')
+    
 
+train_toks = createTokens(mem, 'memory trigger', notMem, 'not')  ##createMemTokens()
+tt_sample = train_toks[200:400]
+tt_sample.extend(train_toks[600:800])
+BMFE = nltk.BinaryMaxentFeatureEncoding(labels=labels,mapping=mapping)
+model = nltk.MaxentClassifier.train(tt_sample,encoding=BMFE,labels=labels,max_iter=3)
+mAcc = nltk.classify.util.accuracy(model, [(noteFeatures(nn),'memory trigger') for nn in mem[:200]])
+nmAcc = nltk.classify.util.accuracy(model, [(noteFeatures(nn),'not') for nn in notMem[280:480]])
+print("Accuracy with mem trig & not mem trig = %s & %s" % (mAcc,  nmAcc))
 
-## Training tokens: List of tuples of (feature dict,string  label)
-## ie: train_toks = [({featureset}, "reference"), ... ]
+model.show_most_informative_features(n=40)
+dummyvar = input("Next?")
+model.show_most_informative_features(n=40, show='pos')
 
 
 
